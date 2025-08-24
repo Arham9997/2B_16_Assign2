43cc3c1c45250101e481002b6797d23f21a31dc4
Arham9997 <arhamahmed030@gmail.com>
2025-08-24 17:16:23 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def transpose(self, mat):
+        new_mat=[]
+        
+        for i in range(n):
+            a=[]
+            for j in range(n):
+                a.append(matrix[j][i])
+            new_mat.append(a)
+        return new_mat
